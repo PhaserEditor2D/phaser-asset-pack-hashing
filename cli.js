13096366ec8ff37e0a96839364421347d6a90d5c
PhaserEditor2D <developers@phasereditor2d.com>
2023-09-12 23:49:14 -0400

    Adds new version.

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ import { AssetPackProcessor } from "./index.js"
 import commandLineArgs from "command-line-args"
 import commandLineUsage from "command-line-usage"
 
-const VERSION = "1.0.3"
+const VERSION = "1.0.5"
 
 const args = commandLineArgs([
     { name: "root", alias: "r", type: String, multiple: false },
